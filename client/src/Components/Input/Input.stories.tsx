2aed7579f8f58eefd685b654f92a3e789d82990d
Kyun Heo <kyun2dot@gmail.com>
2021-08-11 12:03:50 +0900

    :zap: feat: input 컴포넌트 추가

---
+++ client/src/Components/Input/Input.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Input from './';
+
+export default {
+  title: '컴포넌트/인풋',
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  type: 'text',
+  placeholder: '로그인',
+  name: '인풋 이름',
+};

e80e0f3fa91d51946585f61c003d25a917542151
KG <acrkdduf@naver.com>
2021-08-11 19:16:17 +0900

    [#17] :zap: feat: storybook style-components 관련 Provider 적용

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,23 +1,14 @@
 const { addDecorator } = require('@storybook/react');
-const { makeDecorator } = require('@storybook/addons');
 const { jsxDecorator } = require('storybook-addon-jsx');
 const {
   withThemesProvider,
 } = require('storybook-addon-styled-component-theme');
 const { ThemeProvider } = require('styled-components');
 const { lightMode, darkMode } = require('../src/styles/theme');
-const GlobalStyle = require('../src/styles/globalStyle');
 
-// const themes = [lightMode, darkMode];
-// addDecorator(withThemesProvider(themes), ThemeProvider);
+const themes = [lightMode, darkMode];
+addDecorator(withThemesProvider(themes), ThemeProvider);
 addDecorator(jsxDecorator);
-
-addDecorator((story) => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
-));
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

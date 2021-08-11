67d6f5537deff258b0a6f9ed0c0cb7b27185410e
KG <acrkdduf@naver.com>
2021-08-11 11:03:10 +0900

    [#14] :wrench: chore: 동적 라우팅 및 useParams 훅 테스팅

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { StyledDetail } from './styles';
+import * as S from './styles';
 import Stationery from '@/assets/stationery.svg';
+import { useParams } from '@/core/Router';
 
 const Detail = () => {
+  const { params } = useParams();
+  console.log(params);
+
   return (
-    <StyledDetail>
-      <h1>이곳은 디테일 페이지 영역</h1>
+    <S.Detail>
+      <h1>이곳은 디테일 페이지 영역 #{params.id}</h1>
       <p>안녕하세요 디테일 입니다</p>
       <Stationery />
-    </StyledDetail>
+    </S.Detail>
   );
 };
 
--- client/src/pages/Detail/styles.ts
+++ client/src/pages/Detail/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledDetail = styled.div`
+export const Detail = styled.div`
   font-size: 1.5rem;
   background-color: #aaabbb;
   padding: 5rem;

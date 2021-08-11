c1e621755fd5311eea806ef53cb1aed231878319
KG <acrkdduf@naver.com>
2021-08-11 10:59:04 +0900

    [#14] :wrench: chore: Switch 테스트 적용

--- client/src/layouts/App/App.tsx
+++ client/src/layouts/App/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Header from '@/components/Header';
-import { Route } from '@/core/Router';
+import { Route, Switch } from '@/core/Router';
 import Main from '@/pages/Main';
 import Detail from '@/pages/Detail';
 import Banner from '@/components/Banner';
 import Footer from '@/components/Footer';
+import NavLinks from '@/pages/NavLinks';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Banner />
-      <Route path="/main" component={Main} />
-      <Route path="/detail" component={Detail} />
+      <Switch>
+        <Route path="/main" component={Main} />
+        <Route path="/detail/:id" component={Detail} />
+        <Route path="/navlink" component={NavLinks} />
+      </Switch>
       <Footer />
     </div>
   );

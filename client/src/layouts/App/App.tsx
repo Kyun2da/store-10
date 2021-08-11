421ece7118f1a5ac8254d72d0dd0ec4b93123134
KG <acrkdduf@naver.com>
2021-08-11 17:29:04 +0900

    [#10] :nail_care: style: ThemeProvider 적용(다크/라이트테마)

--- client/src/layouts/App/App.tsx
+++ client/src/layouts/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@/components/Header';
 import { Route, Switch } from '@/core/Router';
 import Main from '@/pages/Main';
@@ -6,19 +6,30 @@ import Detail from '@/pages/Detail';
 import Banner from '@/components/Banner';
 import Footer from '@/components/Footer';
 import NavLinks from '@/pages/NavLinks';
+import { ThemeProvider } from 'styled-components';
+import { lightMode, darkMode } from '@/styles/theme';
 
 const App = () => {
+  const [theme, setTheme] = useState('light-mode');
+  const themeMode = theme === 'light-mode' ? lightMode : darkMode;
+
+  const toggleMode = () =>
+    setTheme(theme === 'light-mode' ? 'dark-mode' : 'light-mode');
+
   return (
-    <div>
-      <Header />
-      <Banner />
-      <Switch>
-        <Route path="/main" component={Main} />
-        <Route path="/detail/:id" component={Detail} />
-        <Route path="/navlink" component={NavLinks} />
-      </Switch>
-      <Footer />
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <div>
+        <button onClick={toggleMode}>모드전환 버튼</button>
+        <Header />
+        <Banner />
+        <Switch>
+          <Route path="/main" component={Main} />
+          <Route path="/detail/:id" component={Detail} />
+          <Route path="/navlink" component={NavLinks} />
+        </Switch>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 };
 

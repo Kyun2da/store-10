be877f6809d560166bea124d2544e148b9ae839a
KG <acrkdduf@naver.com>
2021-08-11 11:03:22 +0900

    [#14] :wrench: chore: styled-components 관련 컨벤션 적용

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { stationary } from '@/assets';
-import { StyledMain } from './styles';
+import { useHistory } from '@/core/Router';
+import * as S from './styles';
 
 const Main = () => {
+  const history = useHistory();
+  console.log(history);
+
   return (
-    <StyledMain>
+    <S.Main>
       <h1>이곳은 메인 영역</h1>
       <p>React Frontend boilerplate</p>
       <img src={stationary} alt="default" />
-    </StyledMain>
+    </S.Main>
   );
 };
 

c31ab48c00f69ca432461cbcda7dc8a925712aa5
Kyun Heo <kyun2dot@gmail.com>
2021-08-12 16:28:17 +0900

    :zap: feat: 버튼 컴포넌트 추가

---
+++ client/src/components/Button/styles.ts
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components';
+import { IButtonProps } from './Button';
+
+export const Button = styled.button<IButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  transition: 0.3s;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: black;
+
+  ${(props) => {
+    if (props.size === 'Large') {
+      return css`
+        font-size: 2rem;
+        padding: 1rem 2rem;
+      `;
+    } else if (props.size === 'Small') {
+      return css`
+        font-size: 0.5rem;
+        padding: 0.25rem 0.5rem;
+      `;
+    } else {
+      return css`
+        font-size: 1rem;
+        padding: 0.5rem 1rem;
+      `;
+    }
+  }}
+
+  ${(props) => {
+    if (props.color === 'primary') {
+      return css`
+        background-color: ${props.theme.color.primary};
+        &:hover {
+          background-color: ${props.theme.color.primary3};
+        }
+        &:active {
+          background: ${props.theme.color.primary3};
+        }
+      `;
+    } else if (props.color === 'black') {
+      return css`
+        background-color: #333333;
+        &:hover {
+          background-color: black;
+        }
+        &:active {
+          background: black;
+        }
+      `;
+    }
+  }};
+`;

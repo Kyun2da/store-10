067bc60737c281bb02adf666b7a512f478c83642
KG <acrkdduf@naver.com>
2021-08-12 22:58:28 +0900

    [#39] :nail_care: style: Footer 컴포넌트 라이트/다크모드 색상 추가

--- client/src/styles/styled.d.ts
+++ client/src/styles/styled.d.ts
@@ -7,6 +7,7 @@ declare module 'styled-components' {
     color: {
       'text-color': string;
       'body': string;
+      'footer': string;
       'label': string;
       'placeholder': string;
       'line': string;

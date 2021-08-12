445aa29d7782ceb2bfbc25332b1741214243be1a
김한철 <05.hanch.it@gmail.com>
2021-08-12 15:05:51 +0900

    :nail_care: style : App style 임시 주석처리

--- client/src/layouts/App/styles.ts
+++ client/src/layouts/App/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const RootWrapper = styled.div`
-  padding: 2rem;
+  /* padding: 2rem; */
   background-color: ${({ theme }) => theme.color.body};
-  max-width: 1200px;
-  margin: auto;
+  /* max-width: 1200px; */
+  /* margin: auto; */
 `;

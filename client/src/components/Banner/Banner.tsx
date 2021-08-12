bfe963f24105b50674a31db8a1306b68363f7f5b
KG <acrkdduf@naver.com>
2021-08-12 16:55:08 +0900

    [#31] :zap: feat: Banner 컴포넌트 carousel 적용

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import * as S from './styles';
 
+const dummy = [
+  {
+    id: 'banner-image01',
+    src: 'https://store-10.s3.ap-northeast-2.amazonaws.com/test/banner/banner1.gif',
+  },
+  {
+    id: 'banner-image02',
+    src: 'https://store-10.s3.ap-northeast-2.amazonaws.com/test/banner/banner2.gif',
+  },
+  {
+    id: 'banner-image03',
+    src: 'https://store-10.s3.ap-northeast-2.amazonaws.com/test/banner/banner3.gif',
+  },
+];
+
 const Banner = () => {
+  const renderIndicator = (
+    clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+    isSelected: boolean
+  ) => <S.Indicator onClick={clickHandler} isSelected={isSelected} />;
+
   return (
-    <>
-      <S.Banner className="pc_resolution">
-        이곳은 PC 전용 배너입니다
-        <p>1050px 해상도 부터 출력</p>
-      </S.Banner>
-      <S.Banner className="tablet_resolution">
-        이곳은 태블릿 전용 배너입니다
-        <p>768px 해상도 부터 출력</p>
-      </S.Banner>
-      <S.Banner className="phone_resolution">
-        이곳은 모바일 전용 배너입니다
-        <p>425px 해상도 부터 출력</p>
-      </S.Banner>
-    </>
+    <S.Carousel>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        emulateTouch
+        showArrows={false}
+        showThumbs={false}
+        showStatus={false}
+        renderIndicator={(clickHandler, isSelected) =>
+          renderIndicator(clickHandler, isSelected)
+        }
+      >
+        {dummy.map((d) => (
+          <S.Banner key={d.id}>
+            <img src={d.src} alt="배너이미지" />
+          </S.Banner>
+        ))}
+      </Carousel>
+    </S.Carousel>
   );
 };
 
--- client/src/components/Banner/styles.ts
+++ client/src/components/Banner/styles.ts
@@ -1,15 +1,36 @@
 import styled from 'styled-components';
 
-export const Banner = styled.div`
-  padding: 2rem;
-  text-align: center;
-  ${({ theme }) => theme.fontSize.xxl};
-  ${({ theme }) => theme.fontWeight.s};
-  background-color: ${({ theme }) => theme.color.body};
-  color: ${({ theme }) => theme.color['text-color']};
-  border: 1px solid ${({ theme }) => theme.color.line};
+export const Carousel = styled.div`
+  ul.control-dots {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
+`;
 
-  p {
-    ${({ theme }) => theme.fontSize.m};
+export const Banner = styled.div`
+  width: 100%;
+  height: 25rem;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+interface IIndicator {
+  isSelected: boolean;
+}
+
+export const Indicator = styled.div<IIndicator>`
+  width: 0.6rem;
+  height: 0.6rem;
+  border-radius: 0.6rem;
+  cursor: pointer;
+  background-color: ${({ theme, isSelected }) =>
+    isSelected ? theme.color.primary : '#fff'};
+
+  &:hover {
+    opacity: 0.85;
   }
 `;

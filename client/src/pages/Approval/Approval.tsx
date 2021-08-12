d97f602297fdab237d7e2a7558ad54338f076471
Kyun Heo <kyun2dot@gmail.com>
2021-08-13 02:01:55 +0900

    :zap: feat: 약관 동의 페이지 추가

---
+++ client/src/pages/Approval/Approval.tsx
@@ -0,0 +1,59 @@
+import Button from '@/components/Button';
+import Checkbox from '@/components/Checkbox';
+import Title from '@/components/Title';
+import { useHistory } from '@/core/Router';
+import {
+  PERSONAL_INFO_TEXT,
+  TERMS_OF_SERVICE,
+} from '@/utils/constant/approval';
+import { SITE_TITLE } from '@/utils/constant/common';
+import React from 'react';
+import * as S from './styles';
+
+const Approval = () => {
+  const { historyPush } = useHistory();
+
+  const historyChange = (path: string) => {
+    historyPush(path);
+  };
+  return (
+    <S.ApprovalContainer>
+      <Title level={2}>약관 동의</Title>
+      <S.LabelContainer>
+        <Checkbox />
+        <Title level={5}>
+          {SITE_TITLE}의 모든 약관을 확인하고 전체 동의합니다. (전체동의,
+          선택항목도 포함됩니다.)
+        </Title>
+      </S.LabelContainer>
+      <S.LabelContainer>
+        <Checkbox />
+        <Title level={5}>(필수) 이용약관</Title>
+      </S.LabelContainer>
+      <S.ApprovalBox>{TERMS_OF_SERVICE}</S.ApprovalBox>
+      <S.LabelContainer>
+        <Checkbox />
+        <Title level={5}>(필수) 개인정보 수집 및 이용</Title>
+      </S.LabelContainer>
+      <S.ApprovalBox>{PERSONAL_INFO_TEXT}</S.ApprovalBox>
+      <S.ButtonContainer>
+        <Button
+          type="button"
+          color="white"
+          onClick={() => historyChange('/select_auth')}
+        >
+          이전단계
+        </Button>
+        <Button
+          type="button"
+          color="primary"
+          onClick={() => historyChange('/signup')}
+        >
+          다음단계
+        </Button>
+      </S.ButtonContainer>
+    </S.ApprovalContainer>
+  );
+};
+
+export default Approval;

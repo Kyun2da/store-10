1da28bd6fb7f7dd44c432c8d354504960c9adec1
KangyeolLee <48883344+KangyeolLee@users.noreply.github.com>
2021-08-12 10:54:28 +0900

    [bug] :bug:  bug: 대소문자 구분으로 인해 불필요한 디렉토리 삭제

--- client/src/Components/Figma/Figma.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FigmaButton = () => {
-  return <div>피그마</div>;
-};
-
-export default FigmaButton;
--- client/src/Components/Footer/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Footer = styled.div`
-  font-size: 1.5rem;
-  background-color: #acacac;
-  padding: 2rem;
-`;

99f3f4e081a4eef52c49a28e5e55eb2d34b7867b
KG <acrkdduf@naver.com>
2021-08-12 16:59:34 +0900

    [#31] :nail_care: style: Banner height 크기 수정

--- client/src/components/Banner/styles.ts
+++ client/src/components/Banner/styles.ts
@@ -10,7 +10,7 @@ export const Carousel = styled.div`
 
 export const Banner = styled.div`
   width: 100%;
-  height: 25rem;
+  height: 35rem;
 
   img {
     height: 100%;
@@ -23,9 +23,9 @@ interface IIndicator {
 }
 
 export const Indicator = styled.div<IIndicator>`
-  width: 0.6rem;
-  height: 0.6rem;
-  border-radius: 0.6rem;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 1rem;
   cursor: pointer;
   background-color: ${({ theme, isSelected }) =>
     isSelected ? theme.color.primary : '#fff'};

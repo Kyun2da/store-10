c7764102e917dd6662935f8f9e8d6b37af79885c
Kyun Heo <kyun2dot@gmail.com>
2021-08-14 00:24:55 +0900

    :gear: wip: 자동배포 테스트

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.use('/user', userRoutes);
 
 router.get('/', (req, res) => {
-  res.send('슑과잌붸흐벤 서버에 오신것을 환영합니다!');
+  res.send('슑과잌붸흐벤 서버에 오신것을 환영합니다!!');
 });
 
 export default router;

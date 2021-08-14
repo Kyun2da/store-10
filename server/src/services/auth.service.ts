2ada587662992f41e7ce2e993d576fad24895705
김한철 <05.hanch.it@gmail.com>
2021-08-14 17:27:15 +0900

    [#48] :zap: feat: bcrypt -> bcrypt-nodejs

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { User } from '@/entities/user.entity';
 import GitHubAPI from '@/repositories/oauth/git.oauth';
 import UserRepository from '@/repositories/user.repository';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt-nodejs';
 class AuthService {
   async getGitAccessToken(code: string) {
     const { access_token: accessToken } = await GitHubAPI.getAccessInfo(code);
@@ -16,7 +16,7 @@ class AuthService {
   async Login(userID: string, password: string) {
     const user = await UserRepository().findUserById(userID);
     if (user && user.password && !user.is_oauth) {
-      const passwordCheck = await bcrypt.compare(password, user.password);
+      const passwordCheck = await bcrypt.compareSync(password, user.password);
       if (passwordCheck) {
         const { name, id, refreshToken } = user;
         return { name, id, refreshToken };

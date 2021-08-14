1df002a08f2e1349f219457a90a48c028fd538fd
김한철 <05.hanch.it@gmail.com>
2021-08-14 13:37:04 +0900

    :zap: feat : async용 utile 함수 추가
    try catch 반복 줄이기 위해 사용

---
+++ server/src/utils/wrapAsync.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+const wrapAsync = (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (typeof fn === 'function') {
+      fn(req, res, next).catch(next);
+    }
+  };
+};
+
+export default wrapAsync;

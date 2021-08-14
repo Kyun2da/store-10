00afdff653b5ee4e6a7ab77f0ef620bbd8040532
KG <acrkdduf@naver.com>
2021-08-15 05:32:05 +0900

    [#61] :nail_care: style: Main 컴포넌트 스타일 수정

--- client/src/pages/Main/styles.ts
+++ client/src/pages/Main/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const Main = styled.div`
-  margin-top: 3rem;
-
   h1.product-title {
     color: ${({ theme }) => theme.color['text-color']};
     ${({ theme }) => theme.fontSize.l};

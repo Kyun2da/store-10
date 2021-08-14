87dba1a4a0a38912bc8e73734482e9196d9fd59c
KG <acrkdduf@naver.com>
2021-08-15 05:32:52 +0900

    [#61] :zap: feat: 상품상세 페이지 - 배송안내 컴포넌트 기본 마크업 및 스토리북 코드 작성

---
+++ client/src/components/ProductDetails/ProductDelivery/ProductDelivery.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ProductDelivery from './';
+
+export default {
+  title: '컴포넌트/상품상세-배송안내',
+  component: ProductDelivery,
+} as ComponentMeta<typeof ProductDelivery>;
+
+const Template: ComponentStory<typeof ProductDelivery> = () => (
+  <ProductDelivery />
+);
+
+export const Default = Template.bind({});

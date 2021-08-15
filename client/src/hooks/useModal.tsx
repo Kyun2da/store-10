acdf655aa7518932380965977d72e6eb6896ff35
KG <acrkdduf@naver.com>
2021-08-15 17:23:49 +0900

    [#72] :sparkels: refactor: useModal 리턴 타입을 Array로 변환

--- client/src/hooks/useModal.tsx
+++ client/src/hooks/useModal.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 
-export const useModal = (defaultState: boolean) => {
+const useModal = (defaultState: boolean) => {
   const [isOpen, setIsOpen] = useState(defaultState);
   const toggleModal = () => {
     setIsOpen((prev) => !prev);
   };
 
-  return { isOpen, toggleModal };
+  return [isOpen, toggleModal] as const;
 };
+
+export default useModal;

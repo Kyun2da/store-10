c7a7dd172452d7e7366998c2ce2405e2d42c6932
dongqui <dongqui9@gmail.com>
2021-08-15 22:26:10 +0900

    [#81] :sparkles: refactor: 스토리북 컴포넌트 이름 수정

--- client/src/components/Addresses/Addresses.stories.tsx
+++ client/src/components/Addresses/Addresses.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Addresses from './';
 
 export default {
-  title: '컴포넌트/배너',
+  title: '컴포넌트/배송지 관리',
   component: Addresses,
 } as ComponentMeta<typeof Addresses>;
 

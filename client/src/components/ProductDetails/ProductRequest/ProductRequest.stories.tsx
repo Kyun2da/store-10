47ee15ce5a0a2a629d363d99fc99ae0f1cab90e2
KG <acrkdduf@naver.com>
2021-08-16 01:53:30 +0900

    [#61] :zap: feat: ProductRequest(상품문의) 컴포넌트 기본 구조 작성

---
+++ client/src/components/ProductDetails/ProductRequest/ProductRequest.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ProductRequest from './';
+
+export default {
+  title: '컴포넌트/상품상세-상품문의',
+  component: ProductRequest,
+} as ComponentMeta<typeof ProductRequest>;
+
+const Template: ComponentStory<typeof ProductRequest> = () => (
+  <ProductRequest />
+);
+
+export const Default = Template.bind({});
---
+++ client/src/components/ProductDetails/ProductRequest/ProductRequest.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from '../styles';
+
+const ProductRequest = () => {
+  return <div></div>;
+};
+
+export default ProductRequest;

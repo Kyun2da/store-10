a223ca4d03cb078ba116414a04ae073583a43421
KG <acrkdduf@naver.com>
2021-08-16 20:06:38 +0900

    [#91] :sparkles: refactor: ReviewModal 기본 마크업 구조 및 스타일 적용
    - 간단한 파일 업로더 적용 : 파일 업로드 및 프리뷰 (추후 커스텀 훅으로 분리 필요)
    - textarea : 추후 Input 컴포넌트에 따로 독립된 컴포넌트 단위로 병합 필요
    - Rating 직접 별점을 매길 수 있는 컴포넌트로서의 기능 추가 필요

--- client/src/components/Shared/Modal/styles.ts
+++ client/src/components/Shared/Modal/styles.ts
@@ -33,3 +33,64 @@ export const ModalButton = styled.button`
     opacity: 0.75;
   }
 `;
+
+// 나중에 Input 컴포넌트로 분리할 계획
+export const Form = styled.form`
+  h5 {
+    margin-bottom: 1.5rem;
+  }
+
+  .preview-wrapper {
+    margin-top: 2rem;
+    display: flex;
+    gap: 2rem;
+
+    img {
+      object-fit: cover;
+      width: 20%;
+    }
+  }
+
+  .input-file-button {
+    border-radius: 2rem;
+    padding: 2rem;
+    background-color: #ededed;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    justify-content: center;
+    align-items: center;
+    border: 1px dashed #aaa;
+    cursor: pointer;
+
+    .helper-text {
+      color: #adadad;
+    }
+
+    &:hover {
+      opacity: 0.75;
+    }
+  }
+
+  .error-text {
+    margin-top: 1rem;
+    text-align: right;
+    color: ${({ theme }) => theme.color.error};
+  }
+
+  textarea {
+    width: 100%;
+    height: 15rem;
+    padding: 2rem;
+    min-height: 6rem;
+    max-height: 20rem;
+    border-top-left-radius: 2rem;
+    border-top-right-radius: 2rem;
+    border-bottom-left-radius: 2rem;
+    resize: vertical;
+  }
+
+  & + & {
+    margin-top: 3rem;
+  }
+`;

9f581e6d8554857f2ea4e812ddf26e40671ece4a
KG <acrkdduf@naver.com>
2021-08-17 03:59:22 +0900

    [#103] :sparkles: refactor: 리뷰모달 카테고리 셀렉터 컴포넌트 임포트하여 사용하는 방식으로 변경

--- client/src/components/Shared/Modal/RequestModal.tsx
+++ client/src/components/Shared/Modal/RequestModal.tsx
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@/components/Shared/Button';
 import { Input } from '@/components/Shared/Input';
 import Title from '@/components/Shared/Title';
 import ModalLayout from './ModalLayout';
 import * as S from './styles';
-import { ExchageSVG, GiftSVG, PackageSVG } from '@/assets/svgs';
 import { Textarea } from '@/components/Shared/Input';
 import Form from '@/components/Shared/Form';
+import CategorySelector from '@/components/CategorySelector';
 
 interface RequestModalProps {
   toggleModal: () => void;
 }
 
 const RequestModal = ({ toggleModal }: RequestModalProps) => {
-  const [categoryId, setCategoryId] = useState('category-1');
-
-  const handleClickOnCategory = (e: React.MouseEvent) => {
-    const target = (e.target as HTMLElement).closest('.each-category');
-    setCategoryId(target?.id ?? 'category-1');
-  };
-
   return (
     <ModalLayout toggleModal={toggleModal}>
       <S.ModalHeader>상품문의 작성</S.ModalHeader>
@@ -28,41 +21,7 @@ const RequestModal = ({ toggleModal }: RequestModalProps) => {
         <Form gap={2}>
           <div className="input-wrapper">
             <Title level={5}>문의 유형</Title>
-            <div className="category-selection">
-              <div
-                id="category-1"
-                className={
-                  'each-category' +
-                  (categoryId === 'category-1' ? ' selected' : '')
-                }
-                onClick={handleClickOnCategory}
-              >
-                <GiftSVG width={60} height={60} />
-                <span className="category-text">상품관련</span>
-              </div>
-              <div
-                id="category-2"
-                className={
-                  'each-category' +
-                  (categoryId === 'category-2' ? ' selected' : '')
-                }
-                onClick={handleClickOnCategory}
-              >
-                <PackageSVG width={60} height={60} />
-                <span className="category-text">배송관련</span>
-              </div>
-              <div
-                id="category-3"
-                className={
-                  'each-category' +
-                  (categoryId === 'category-3' ? ' selected' : '')
-                }
-                onClick={handleClickOnCategory}
-              >
-                <ExchageSVG width={60} height={60} />
-                <span className="category-text">교환/환불관련</span>
-              </div>
-            </div>
+            <CategorySelector />
           </div>
           <div className="input-wrapper">
             <Title level={5}>문의 제목</Title>

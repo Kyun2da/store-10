d0d34ea5788b766d95ed74e2711587e83d33f29f
KG <acrkdduf@naver.com>
2021-08-16 23:34:39 +0900

    [#91] :zap: feat: 리뷰 관련 모둘 스토리북 코드 작성

---
+++ client/src/components/Shared/Modal/RequestModal.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import RequestModal from './RequestModal';
+
+export default {
+  title: '컴포넌트/공통/문의모달',
+  component: RequestModal,
+} as ComponentMeta<typeof RequestModal>;
+
+const Template: ComponentStory<typeof RequestModal> = (args) => (
+  <RequestModal {...args} />
+);
+
+export const Default = Template.bind({});

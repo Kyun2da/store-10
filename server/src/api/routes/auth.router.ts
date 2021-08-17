06054dcada4ddfa2693c8561c476e48b67fd5b19
허균 <kyun2dot@gmail.com>
2021-08-18 00:36:11 +0900

    [#112] :zap: feat: 유저 정보 api 추가

--- server/src/api/routes/auth.router.ts
+++ server/src/api/routes/auth.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import wrapAsync from '@/utils/wrapAsync';
 import AuthController from '@/controllers/auth.controller';
+import authJWT from '../middlewares/auth.middlewares';
 
 const router = Router();
 
 router.get('/', AuthController.gitOAuthUrl);
 router.post('/login', wrapAsync(AuthController.Login));
 router.get('/callback', wrapAsync(AuthController.callback));
+router.get('/check', authJWT, wrapAsync(AuthController.check));
 
 export default router;

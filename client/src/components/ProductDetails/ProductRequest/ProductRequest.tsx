839896163c99b4b355420493b99ba8b8abf737b3
KG <acrkdduf@naver.com>
2021-08-17 21:57:52 +0900

    [#110] :sparkles: refactor: 기존 Table 컴포넌트에서 Collapse 컴포넌트로 변경'

--- client/src/components/ProductDetails/ProductRequest/ProductRequest.tsx
+++ client/src/components/ProductDetails/ProductRequest/ProductRequest.tsx
@@ -1,10 +1,10 @@
 import Button from '@/components/Shared/Button';
-import Table from '@/components/Shared/Table';
 import Title from '@/components/Shared/Title';
 import React from 'react';
 import * as S from '../styles';
 import useModal from '@/hooks/useModal';
 import { RequestModal } from '@/components/Shared/Modal';
+import Collapse from '@/components/Shared/Collapse';
 
 const headers = [
   { name: '번호', value: 'number' },
@@ -22,6 +22,8 @@ const items = [
     username: '우아한개발자',
     date: '2021-08-21',
     check: '미답변',
+    content: '재구매 하고 싶어요! 재구매 해주세요! 우와아아으앙!',
+    answer: '',
   },
   {
     id: 2,
@@ -30,6 +32,9 @@ const items = [
     username: '우아한퀄리티',
     date: '2021-08-21',
     check: '미답변',
+    content:
+      '안녕하세요 품질 퀄리티 문의 드립니다. 이게 품질이 그렇게까지 좋은거 같지 않더라구요. 그나저나 제가 98년 LA에 있을 때도 품질 때문에 크게 싸운적이 있는데',
+    answer: '',
   },
   {
     id: 3,
@@ -38,6 +43,9 @@ const items = [
     username: '우아한불편러',
     date: '2021-08-21',
     check: '완료',
+    content:
+      '아니 선생님 장사를 이렇게 하면 됩니까 이거? 분명 10개를 샀는데 왜 2개밖에 안 오는거죠? 신고감이네요 ㅂㅂ',
+    answer: '안녕하세요 배민문방구 입니다. 감사합니다.',
   },
   {
     id: 4,
@@ -46,6 +54,10 @@ const items = [
     username: '우아한매니저',
     date: '2021-08-21',
     check: '완료',
+    content:
+      '안녕하세요 입점 문의 드립니다. 뭐라고 해야할 지는 몰라서 그냥 길 게 써 볼려구요 하하하. 근데 뭐라고 쓸 말이 없네요~ 입점 문의 해주시죠 그냥! 해주세요~~ 하하하 호호호~ 무슨 말을 더 써야할 지 모르겠으니 이만 글을 줄이도록 하겠습니다. 만수무강 하시고 하시는 사업 모두 번창하시기를 바랍니다.안녕하세요 입점 문의 드립니다. 뭐라고 해야할 지는 몰라서 그냥 길 게 써 볼려구요 하하하. 근데 뭐라고 쓸 말이 없네요~ 입점 문의 해주시죠 그냥! 해주세요~~ 하하하 호호호~ 무슨 말을 더 써야할 지 모르겠으니 이만 글을 줄이도록 하겠습니다. 만수무강 하시고 하시는 사업 모두 번창하시기를 바랍니다.안녕하세요 입점 문의 드립니다. 뭐라고 해야할 지는 몰라서 그냥 길 게 써 볼려구요 하하하. 근데 뭐라고 쓸 말이 없네요~ 입점 문의 해주시죠 그냥! 해주세요~~ 하하하 호호호~ 무슨 말을 더 써야할 지 모르겠으니 이만 글을 줄이도록 하겠습니다. 만수무강 하시고 하시는 사업 모두 번창하시기를 바랍니다.안녕하세요 입점 문의 드립니다. 뭐라고 해야할 지는 몰라서 그냥 길 게 써 볼려구요 하하하. 근데 뭐라고 쓸 말이 없네요~ 입점 문의 해주시죠 그냥! 해주세요~~ 하하하 호호호~ 무슨 말을 더 써야할 지 모르겠으니 이만 글을 줄이도록 하겠습니다. 만수무강 하시고 하시는 사업 모두 번창하시기를 바랍니다.',
+    answer:
+      '안녕하세요 배민문방구 입니다. 감사합니다. 입점 문의는 02-2173-2193번으로 전화 주시기 바랍니다. 감사합니다.',
   },
 ];
 
@@ -68,7 +80,7 @@ const ProductRequest = () => {
         </Button>
       </S.TopArea>
 
-      <Table fullWidth headers={headers} items={items} />
+      <Collapse headers={headers} items={items} gaps="1fr 3fr 1fr 1fr 1fr" />
 
       {isOpen && <RequestModal toggleModal={toggleModal} />}
     </S.PanelWrapper>

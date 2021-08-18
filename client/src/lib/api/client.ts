33d336959f189fcddf6af73f7f008bc754788c1a
KG <acrkdduf@naver.com>
2021-08-18 22:54:13 +0900

    [#131] :wrench: chore: env 파일을 이용해 API ENDPOINT 숨김 처리

--- client/src/lib/api/client.ts
+++ client/src/lib/api/client.ts
@@ -4,7 +4,6 @@ const client = axios.create({
   withCredentials: true,
 });
 
-client.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? '/' : 'http://3.38.92.37:3000';
+client.defaults.baseURL = process.env.SERVER_API_HOST;
 
 export default client;

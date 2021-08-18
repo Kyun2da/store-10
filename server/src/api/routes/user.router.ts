9c4b3fcc27e204226f5d076208f9442818278e52
허균 <kyun2dot@gmail.com>
2021-08-19 04:07:44 +0900

    [#111] :zap: feat: 일반 유저 회원가입 api 만들기

--- server/src/api/routes/user.router.ts
+++ server/src/api/routes/user.router.ts
@@ -8,16 +8,6 @@ import CreateUserRequest from '@/dtos/user/createUser';
 import { checkUser } from '../middlewares/checkUser.middleware';
 
 const router = Router();
-
-// TODO 아직은 미완성인 상태.
-/**
- * 형태로 넣어주면 유저 생성됨.
-  {
-    "user_id" : "test2",
-    "password" :"test",
-    "name":"iamironman"
-  }
-*/
 router.post(
   '/check',
   invalidRequest(...UserCheckRequest.validators),
---
+++ server/src/dtos/user/createUser.ts
@@ -0,0 +1,38 @@
+import { body } from 'express-validator';
+import { Request } from 'express';
+import { validatePassword } from '@/utils/validate';
+
+export default class CreateUserRequest {
+  static validators = [
+    body('user_id')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('body: user_id가 이메일 형식이 아닙니다.'),
+    body('password')
+      .custom((val: string) => validatePassword(val))
+      .withMessage(
+        'body: password가 10자 이상 영어 대문자, 소문자, 숫자, 특수문자 중 2종류를 조합한 형식이 아닙니다.'
+      ),
+    body('rePassword')
+      .notEmpty()
+      .withMessage('body: rePassword가 필요합니다.')
+      .custom((val: string, { req }) => {
+        if (val !== req.body.password) return false;
+        return true;
+      })
+      .withMessage('body: password와 rePassword가 다릅니다.'),
+    body('name').notEmpty().withMessage('body: 이름이 필요합니다.'),
+  ];
+
+  user_id: string;
+  password: string;
+  rePassword: string;
+  name: string;
+
+  constructor(req: Request) {
+    this.user_id = req.body.user_id;
+    this.password = req.body.passsword;
+    this.rePassword = req.body.rePassword;
+    this.name = req.body.name;
+  }
+}

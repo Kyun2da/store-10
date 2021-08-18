754febdc9819b5f5785a165a9b21ddceaf262769
허균 <kyun2dot@gmail.com>
2021-08-19 04:04:17 +0900

    [#111] :sparkles: refactor: 중복확인 미들웨어 따로 빼서 사용

---
+++ server/src/api/middlewares/checkUser.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import userRepository from '@/repositories/user.repository';
+import ApiResponse from './response-format';
+import HttpStatusCode from '@/types/statusCode';
+
+export async function checkUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const user = await userRepository().findUserById(req.body.user_id);
+  if (user) {
+    ApiResponse(
+      res,
+      HttpStatusCode.CONFLICT,
+      false,
+      '이미 회원가입이 된 아이디입니다.'
+    );
+  }
+
+  return next();
+}
--- server/src/api/routes/user.router.ts
+++ server/src/api/routes/user.router.ts
@@ -4,6 +4,8 @@ import UserController from '@/controllers/user.controller';
 import wrapAsync from '@/utils/wrapAsync';
 import UserCheckRequest from '@/dtos/user/check';
 import invalidRequest from '@/api/middlewares/invalid-request';
+import CreateUserRequest from '@/dtos/user/createUser';
+import { checkUser } from '../middlewares/checkUser.middleware';
 
 const router = Router();
 
@@ -19,8 +21,15 @@ const router = Router();
 router.post(
   '/check',
   invalidRequest(...UserCheckRequest.validators),
+  checkUser,
   wrapAsync(UserController.checkUserEmail)
 );
-router.post('/', wrapAsync(UserController.createUser));
+
+router.post(
+  '/',
+  invalidRequest(...CreateUserRequest.validators),
+  checkUser,
+  wrapAsync(UserController.createUser)
+);
 
 export default router;

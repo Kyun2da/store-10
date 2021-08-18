8c2ee2e8143f4a51967f755557d7e8cac7720bf7
KG <acrkdduf@naver.com>
2021-08-19 03:44:30 +0900

    [#135] :zap: feat: Error 컴포넌트 작성 및 스토리북 코드 작성

---
+++ client/src/components/Shared/Error/Error.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Error from './Error';
+
+export default {
+  title: '컴포넌트/공통/에러창',
+  component: Error,
+} as ComponentMeta<typeof Error>;
+
+const Template: ComponentStory<typeof Error> = (args) => <Error {...args} />;
+
+export const Default = Template.bind({});

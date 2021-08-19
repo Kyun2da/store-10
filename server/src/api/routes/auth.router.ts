8b4ac929315c7dbdffec778da023c190b3687486
허균 <kyun2dot@gmail.com>
2021-08-19 10:51:20 +0900

    [#111] :sparkles: refactor: auth 미들웨어 이름 변경

--- server/src/api/routes/auth.router.ts
+++ server/src/api/routes/auth.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import wrapAsync from '@/utils/wrapAsync';
 import AuthController from '@/controllers/auth.controller';
 import { oauthError } from '../middlewares/oauth';
-import authJWT from '../middlewares/auth.middlewares';
+import authJWT from '../middlewares/auth.middleware';
 
 const router = Router();
 

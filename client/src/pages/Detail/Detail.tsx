e70881e8d9a015f45743e32d790e94bb8edf7be0
KG <acrkdduf@naver.com>
2021-08-19 18:50:32 +0900

    [#148] :sparkles: refactor: API 호출 시점은 실 데이터가 필요한 컴포넌트 단위에서 하는 것으로 결정

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import * as S from './styles';
-import { useParams } from '@/lib/Router';
 import { ProductDetailTab } from '@/components/Shared/Tab';
 import ProductInfo from '@/components/ProductDetails/ProductInfo';
 
 const Detail = () => {
-  const { params } = useParams();
-  console.log(params);
-
   return (
     <S.Detail className="container">
       <ProductInfo />

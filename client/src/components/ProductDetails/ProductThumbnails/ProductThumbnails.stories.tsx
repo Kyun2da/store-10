79ffb13ebb770f1825bb83758cc2f5523916faa5
KG <acrkdduf@naver.com>
2021-08-21 18:35:11 +0900

    [#106] :sparkles: refactor: 스토리북 react-query 연동 문제로 목업데이터 설정

--- client/src/components/ProductDetails/ProductThumbnails/ProductThumbnails.stories.tsx
+++ client/src/components/ProductDetails/ProductThumbnails/ProductThumbnails.stories.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ProductThumbnails from './ProductThumbnails';
-import { useGetProductById } from '@/hooks/queries/product';
 import thumbnailsParser from '@/utils/thumbnailsParser';
+import { thumbnails } from '@/dummies/mockThumbnails';
 
 export default {
   title: '컴포넌트/상품상세-섬네일',
@@ -11,30 +11,16 @@ export default {
 } as ComponentMeta<typeof ProductThumbnails>;
 
 // react-query 어케 쓰나염
+const [thumbDetails, thumbOrigins, thumbThumbnails] =
+  thumbnailsParser(thumbnails);
 
-const Template: ComponentStory<typeof ProductThumbnails> = (args) => {
-  const { data } = useGetProductById('70000');
-
-  if (data) {
-    const { thumbnails } = data;
-    const [thumbDetails, thumbOrigins, thumbThumbnails] =
-      thumbnailsParser(thumbnails);
-
-    return (
-      <ProductThumbnails
-        thumbDetails={thumbDetails}
-        thumbOrigins={thumbOrigins}
-        thumbThumbnails={thumbThumbnails}
-      />
-    );
-  }
-
-  return <ProductThumbnails {...args} />;
-};
+const Template: ComponentStory<typeof ProductThumbnails> = (args) => (
+  <ProductThumbnails {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  thumbDetails: undefined,
-  thumbOrigins: undefined,
-  thumbThumbnails: undefined,
+  thumbDetails,
+  thumbOrigins,
+  thumbThumbnails,
 };

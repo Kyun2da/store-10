71d6da3b21d413b7e7a4970b7b1393a442bd17b7
KG <acrkdduf@naver.com>
2021-08-22 03:29:14 +0900

    [#179] :zap: feat: 리뷰 별점 / 내용 작성 API 연동 (validation , 파일 미처리)

--- client/src/components/Shared/Modal/ReviewModal.tsx
+++ client/src/components/Shared/Modal/ReviewModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RatingSetter } from '@/components/Shared/Rating';
 import Title from '@/components/Shared/Title';
 import ModalLayout from './ModalLayout';
@@ -7,6 +7,8 @@ import Button from '@/components/Shared/Button';
 import { FileInput, Textarea } from '@/components/Shared/Input';
 import useFileInput from '@/hooks/useFileInput';
 import Form from '@/components/Shared/Form';
+import { useCreateReview } from '@/hooks/queries/product';
+import { useParams } from '@/lib/Router';
 
 interface ReviewModalProps {
   toggleModal: () => void;
@@ -15,6 +17,10 @@ interface ReviewModalProps {
 // TODO: Input 타입을 조금 더 만들어야 하겠군뇨 호호호
 
 const ReviewModal = ({ toggleModal }: ReviewModalProps) => {
+  const { id } = useParams().params;
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState('');
+  const { mutate } = useCreateReview();
   const {
     fileRef,
     fileImgs,
@@ -23,15 +29,36 @@ const ReviewModal = ({ toggleModal }: ReviewModalProps) => {
     handleUploadFile,
   } = useFileInput();
 
+  const handleOnRating = (rating: number) => {
+    setRating(rating);
+  };
+
+  const handleOnTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { target } = e;
+    setContent(target.value);
+  };
+
+  const handleOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    mutate({
+      rating,
+      product_id: id,
+      content,
+    });
+
+    toggleModal();
+  };
+
   return (
     <ModalLayout toggleModal={toggleModal}>
       <S.ModalHeader>상품후기 작성</S.ModalHeader>
       <S.ModalDivider />
       <S.ModalBody>
-        <Form gap={3}>
+        <Form onSubmit={handleOnSubmit} gap={3}>
           <div className="input-wrapper">
             <Title level={5}>별점 매기기</Title>
-            <RatingSetter />
+            <RatingSetter handleOnRating={handleOnRating} />
           </div>
           <Title level={5}>사진 업로드 (최대 3장)</Title>
 
@@ -53,16 +80,18 @@ const ReviewModal = ({ toggleModal }: ReviewModalProps) => {
               placeholder="다른 구매자와 판매자에게 도움이 될 수 있도록 자세하고 솔직하게 리뷰 작성 부탁드려요!"
               resize="vertical"
               name="review-content"
+              onChange={handleOnTextarea}
               fullWidth
             />
           </div>
+
+          <S.ModalButtonArea>
+            <Button color="primary" type="submit" fullWidth>
+              완료
+            </Button>
+          </S.ModalButtonArea>
         </Form>
       </S.ModalBody>
-      <S.ModalButtonArea>
-        <Button color="primary" type="submit" fullWidth>
-          완료
-        </Button>
-      </S.ModalButtonArea>
     </ModalLayout>
   );
 };

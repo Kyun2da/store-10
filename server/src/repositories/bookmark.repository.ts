506ba0c1f3d95d4b81f4263d9d9aecba4bed90a8
허균 <kyun2dot@gmail.com>
2021-08-21 22:41:14 +0900

    :zap: feat: 북마크 api 구현

---
+++ server/src/repositories/bookmark.repository.ts
@@ -0,0 +1,52 @@
+import { Bookmark } from '@/entities/bookmark.entity';
+import {
+  EntityRepository,
+  getCustomRepository,
+  Repository,
+  DeleteResult,
+  In,
+} from 'typeorm';
+
+interface IBookmarkInfo {
+  userId: number;
+  productId: number;
+}
+
+@EntityRepository(Bookmark)
+class BookmarkRepository extends Repository<Bookmark> {
+  createBookmark({ userId, productId }: IBookmarkInfo): Promise<Bookmark> {
+    const Bookmark = this.create({
+      user_id: userId,
+      product_id: productId,
+    });
+    return this.save(Bookmark);
+  }
+
+  getBookmarksDetail(userId: number): Promise<Bookmark[]> {
+    // TODO: productimage 하나만 가져오기
+    const Bookmarks = this.find({
+      where: { user_id: userId },
+      relations: ['product', 'product.productImage'],
+    });
+    return Bookmarks;
+  }
+
+  getBookmarks(userId: number): Promise<Bookmark[]> {
+    const Bookmarks = this.find({
+      where: { user_id: userId },
+    });
+    return Bookmarks;
+  }
+
+  deleteBookmarks({
+    productIds,
+    userId,
+  }: {
+    productIds: number[];
+    userId: number;
+  }): Promise<DeleteResult> {
+    return this.delete({ user_id: userId, product_id: In(productIds) });
+  }
+}
+
+export default () => getCustomRepository(BookmarkRepository);

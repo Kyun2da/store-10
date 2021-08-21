16b7504258ca588c534b0e60e9e3a9d3696d8a7b
KG <acrkdduf@naver.com>
2021-08-22 03:20:11 +0900

    [#179] :zap: feat: 리뷰 조회 및 생성 관련 비동기처리 react-query를 이용해 훅스 처리

--- client/src/hooks/queries/product/index.tsx
+++ client/src/hooks/queries/product/index.tsx
@@ -4,15 +4,39 @@ import {
   getRecentProducts,
   getRecommandProducts,
   getCateogries,
+  getProductReviewsById,
+  getProductReviewsCountById,
+  postProductReview,
 } from '@/lib/api/product';
-import { useQuery } from 'react-query';
-import { ICategory, IProduct, IProductDetail } from '@/types/index';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import {
+  ICategory,
+  IProduct,
+  IProductDetail,
+  IProductReview,
+  IReviewCount,
+} from '@/types/index';
+import { notify } from '@/components/Shared/Toastify';
 
 export const useGetProductById = (id: string) => {
   return useQuery<IProductDetail, Error>(
     ['productDetail', id],
     () => getProductById(id),
     { refetchOnWindowFocus: false }
+  );
+};
+
+export const useGetProductReviewsById = (id: string, offset: number) => {
+  return useQuery<IProductReview[], Error>(
+    ['productReview', id, offset],
+    () => getProductReviewsById(id, offset),
+    { keepPreviousData: true }
+  );
+};
+
+export const useGetProductReviewsCount = (id: string) => {
+  return useQuery<IReviewCount, Error>(['productReviewCount', id], () =>
+    getProductReviewsCountById(id)
   );
 };
 
@@ -32,4 +56,20 @@ export const useGetCateogries = () => {
   return useQuery<ICategory[], Error>('categories', () => getCateogries(), {
     refetchOnWindowFocus: false,
   });
+};
+
+export const useCreateReview = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(postProductReview, {
+    onSuccess: () => {
+      notify('success', '리뷰를 작성했습니다!');
+      queryClient.invalidateQueries('productReview');
+      queryClient.invalidateQueries('productReviewCount');
+    },
+    onError: () => {
+      notify('error', '에러가 발생했습니다!!');
+    },
+  });
+
+  return mutation;
 };

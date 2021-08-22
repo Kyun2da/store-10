f1f1ca224311d5f6c558e6f649f3aa220c9374f5
dongqui <dongqui9@gmail.com>
2021-08-22 12:25:45 +0900

    [#170] :zap: feat: 주문/결제 페이지, 셀렉트 컴포넌트 스토리북 추가

--- client/src/components/Shared/Select/styles.ts
+++ client/src/components/Shared/Select/styles.ts
@@ -4,7 +4,7 @@ export const Select = styled.div`
   position: relative;
   select {
     outline: none;
-    padding: 1.2rem 1.2rem;
+    padding: 1.2rem 1.6rem;
     width: 100%;
     appearance: none;
     border: 1px solid ${({ theme }) => theme.color['border-gray']};

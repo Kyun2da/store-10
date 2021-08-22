dd214cdbd32d8578007865c0a067f3a75cf15f36
KG <acrkdduf@naver.com>
2021-08-22 18:11:14 +0900

    [#179] :zap: feat: 이미지 압축 헬퍼 유틸 함수 구현

--- client/src/lib/api/product/index.ts
+++ client/src/lib/api/product/index.ts
@@ -8,7 +8,6 @@ import {
   IReviewCount,
   IReview,
 } from '@/types';
-import axios from 'axios';
 
 export const getProductById = async (id: string) => {
   return await client.get<IProductDetail>(`/product/${id}`);
--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -13,3 +13,57 @@ export const wonFormat = (number: number | string) => {
 export const dateFormat = (date: string) => {
   return dayjs(date).format('YYYY-MM-DD');
 };
+
+export const compressImageFile = (file: File) => {
+  return new Promise<File>((resolve) => {
+    const fileReader = new FileReader();
+
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = ({ target }) => {
+      if (!target || !target.result) return;
+
+      const blob = new Blob([target.result]);
+      const blobURL = URL.createObjectURL(blob);
+
+      const image = new Image();
+      image.src = blobURL;
+      image.onload = async () => {
+        const resized = await resize(image);
+        resolve(
+          new File([dataURItoBlob(resized)], file.name, {
+            type: 'image/jpeg',
+            lastModified: Date.now(),
+          })
+        );
+      };
+    };
+  });
+};
+
+const resize = async (image: HTMLImageElement) => {
+  const canvas = document.createElement('canvas');
+  const { width, height } = image;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  const context = canvas.getContext('2d');
+  context?.drawImage(image, 0, 0, width, height);
+
+  return canvas.toDataURL('image/jpeg', 0.5);
+};
+
+const dataURItoBlob = (resized: string) => {
+  const data = resized.split(',');
+  const bytes = atob(data[1]); // base64 decoding
+  const mime = data[0].split(':')[1].split(';')[0]; // get MIMEType
+  const arrayBuffer = new ArrayBuffer(bytes.length);
+  const int8Arr = new Uint8Array(arrayBuffer);
+
+  for (let i = 0; i < bytes.length; i++) {
+    int8Arr[i] = bytes.charCodeAt(i);
+  }
+
+  const blob = new Blob([arrayBuffer], { type: mime });
+  return blob;
+};

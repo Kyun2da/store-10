aeb92204ef778757eb79a5dbea90686827c1c4ee
KG <acrkdduf@naver.com>
2021-08-22 17:42:22 +0900

    [#179] :sparkles: refactor: RatingSetter 컴포넌트 디자인 수정 및 validation 추가

--- client/src/components/Shared/Rating/RatingSetter.tsx
+++ client/src/components/Shared/Rating/RatingSetter.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { ErrorMessage } from '@/components/Shared/Input/style';
 import * as S from './styles';
 
 const COUNT_STAR = 5;
 
 interface IRatingSetter {
   handleOnRating: (rating: number) => void;
+  error?: boolean;
+  helpertext?: string;
 }
 
-const RatingSetter = ({ handleOnRating }: IRatingSetter) => {
+const RatingSetter = ({ handleOnRating, error, helpertext }: IRatingSetter) => {
   const [isCheck, setIsCheck] = useState<string[]>([]);
   const [rate, setRate] = useState<number>(0);
 
@@ -32,20 +35,23 @@ const RatingSetter = ({ handleOnRating }: IRatingSetter) => {
   };
 
   return (
-    <S.Rating>
-      {new Array(COUNT_STAR).fill(1).map((star, idx) => (
-        <svg
-          key={star + Math.random().toString(30)}
-          width="25"
-          height="25"
-          className={isCheck[idx]}
-          onClick={() => handleClickOnStar(idx)}
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
-      ))}
-      <S.RatingPoint>{rate}점</S.RatingPoint>
-    </S.Rating>
+    <S.RatingWrapper>
+      <S.Rating>
+        {new Array(COUNT_STAR).fill(1).map((star, idx) => (
+          <svg
+            key={star + Math.random().toString(30)}
+            width="25"
+            height="25"
+            className={isCheck[idx]}
+            onClick={() => handleClickOnStar(idx)}
+          >
+            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+          </svg>
+        ))}
+        <S.RatingPoint>{rate}점</S.RatingPoint>
+      </S.Rating>
+      {error && <ErrorMessage>{helpertext}</ErrorMessage>}
+    </S.RatingWrapper>
   );
 };
 

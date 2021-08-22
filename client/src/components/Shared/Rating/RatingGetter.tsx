cee97b697a2f2be9beeaa2f6acdd4502e68f3c41
KG <acrkdduf@naver.com>
2021-08-22 20:27:17 +0900

    [#179] :zap: feat: 별점 평균 API 연동하여 그릴 수 있도록 변경

--- client/src/components/Shared/Rating/RatingGetter.tsx
+++ client/src/components/Shared/Rating/RatingGetter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import * as S from './styles';
 import { nanoid } from 'nanoid';
 
+const START_COUNT = 5;
+
 interface IRating {
   rating: number;
   uniqueId: string;
@@ -9,7 +11,8 @@ interface IRating {
 
 const RatingGetter = ({ rating, uniqueId }: IRating) => {
   const count = rating >> 0;
-  const rest = rating - count;
+  const pointNumber = +(rating - count).toFixed(1);
+  const rest = START_COUNT - Math.ceil(pointNumber) - count;
 
   // TODO: ㅋㅋ 하드코딩 또 오졌다리
   // 이걸 따로 svg 파일로 관리할 수 있을지 고민중입니다
@@ -26,10 +29,14 @@ const RatingGetter = ({ rating, uniqueId }: IRating) => {
         </svg>
       ))}
 
-      {rest !== 0 ? (
+      {pointNumber !== 0 && (
         <svg width="25" height="25">
           <linearGradient y2="100%" x2="100%" y1="100%" x1="0%" id={uniqueId}>
-            <stop offset={rest * 100 + '%'} stopColor="#2ac1bc" id={uniqueId} />
+            <stop
+              offset={pointNumber * 100 + '%'}
+              stopColor="#2ac1bc"
+              id={uniqueId}
+            />
             <stop offset="0%" stopColor="#ccc" id={uniqueId} />
           </linearGradient>
           <path
@@ -37,16 +44,16 @@ const RatingGetter = ({ rating, uniqueId }: IRating) => {
             d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
           />
         </svg>
-      ) : (
-        new Array(5 - count).fill(1).map((_) => (
-          <svg key={nanoid()} width="25" height="25">
-            <path
-              fill="#ccc"
-              d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
-            />
-          </svg>
-        ))
       )}
+
+      {new Array(rest).fill(1).map((_) => (
+        <svg key={nanoid()} width="25" height="25">
+          <path
+            fill="#ccc"
+            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+          />
+        </svg>
+      ))}
     </S.Rating>
   );
 };

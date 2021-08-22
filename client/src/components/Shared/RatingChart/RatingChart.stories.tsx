74573c3fe9d79aa817059c0b34232d46d9a77ffa
KG <acrkdduf@naver.com>
2021-08-22 20:31:46 +0900

    [#179] :zap: feat: 별점 차트 API 연동하여 그릴 수 있도록 반영

--- client/src/components/Shared/RatingChart/RatingChart.stories.tsx
+++ client/src/components/Shared/RatingChart/RatingChart.stories.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import RatingChart from '.';
+import { IRating } from '@/types';
 
 export default {
   title: '컴포넌트/공통/별점차트',
   component: RatingChart,
 } as ComponentMeta<typeof RatingChart>;
 
-const Template: ComponentStory<typeof RatingChart> = () => <RatingChart />;
+const ratings: IRating[] = [
+  { rating: 5, count: '10' },
+  { rating: 4, count: '9' },
+  { rating: 3, count: '2' },
+  { rating: 2, count: '0' },
+  { rating: 1, count: '1' },
+];
+
+const Template: ComponentStory<typeof RatingChart> = (args) => (
+  <RatingChart {...args} />
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  ratings,
+};
--- client/src/components/Shared/RatingChart/RatingChart.tsx
+++ client/src/components/Shared/RatingChart/RatingChart.tsx
@@ -1,41 +1,24 @@
 import React from 'react';
 import * as S from './styles';
+import { IRating } from '@/types';
+import { nanoid } from 'nanoid';
 
-const RatingChart = () => {
-  // TODO: 퍼센트 계산은 손을 좀 많이 봐야 되어유..
-  // 지금은 야매로다 그냥 대충 했습니다
+interface IRatingChart {
+  ratings: IRating[];
+}
+
+const RatingChart = ({ ratings }: IRatingChart) => {
+  ratings.sort((a, b) => b.rating - a.rating);
 
   return (
     <S.RatingChart>
-      <S.ChartBar count={8}>
-        <div className="bar"></div>
-        <div className="text">5점</div>
-        <span className="hover-text">8명</span>
-      </S.ChartBar>
-
-      <S.ChartBar count={6}>
-        <div className="bar"></div>
-        <div className="text">4점</div>
-        <span className="hover-text">6명</span>
-      </S.ChartBar>
-
-      <S.ChartBar count={1}>
-        <div className="bar"></div>
-        <div className="text">3점</div>
-        <span className="hover-text">1명</span>
-      </S.ChartBar>
-
-      <S.ChartBar>
-        <div className="bar"></div>
-        <div className="text">2점</div>
-        <span className="hover-text">0명</span>
-      </S.ChartBar>
-
-      <S.ChartBar count={1}>
-        <div className="bar"></div>
-        <div className="text">1점</div>
-        <span className="hover-text">1명</span>
-      </S.ChartBar>
+      {ratings.map((rating) => (
+        <S.ChartBar key={nanoid()} count={+rating.count}>
+          <div className="bar"></div>
+          <div className="text">{rating.rating}점</div>
+          <span className="hover-text">{rating.count}명</span>
+        </S.ChartBar>
+      ))}
     </S.RatingChart>
   );
 };

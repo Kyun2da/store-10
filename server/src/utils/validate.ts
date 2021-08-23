cb378c1788868da1fc7c9a2a5bbdc8c27c180e18
dongqui <dongqui9@gmail.com>
2021-08-24 02:59:21 +0900

    [#175 :zap: feat: 배송지 api validation

--- server/src/utils/validate.ts
+++ server/src/utils/validate.ts
@@ -3,3 +3,13 @@ export const validatePassword = (password: string) => {
     /^(?=.*[A-Z])(?=.*[a-z])([^\s]){10,}|(?=.*[A-Z])(?=.*[0-9])([^\s]){10,}|(?=.*[A-Z])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}|(?=.*[a-z])(?=.*[0-9])([^\s]){10,}|(?=.*[a-z])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}|(?=.*[0-9])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}$/;
   return regex.test(password);
 };
+
+export const validatePhone = (phone: string) => {
+  const regex = /[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}/;
+  return regex.test(phone);
+};
+
+export const validatePostcode = (postcode: string) => {
+  const regex = /[0-9]{5,6}/;
+  return regex.test(postcode);
+};

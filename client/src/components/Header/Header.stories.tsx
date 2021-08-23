2d5e77d1555de2c849ad53d7b9d505dafae8647f
김한철 <05.hanch.it@gmail.com>
2021-08-23 18:15:03 +0900

    [#168] :zap: feat: 스토리북 헤더 mock api 추가

--- client/src/components/Header/Header.stories.tsx
+++ client/src/components/Header/Header.stories.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import Header from './';
+import categorylist from '@/dummies/mockCateogrys';
+import { RecoilRoot } from 'recoil';
+import { rest } from 'msw';
+
+const HeaderComponent = () => (
+  <Suspense fallback={<div>loading</div>}>
+    <RecoilRoot>
+      <Header />
+    </RecoilRoot>
+  </Suspense>
+);
 
 export default {
   title: '컴포넌트/헤더',
-  component: Header,
+  component: HeaderComponent,
   parameters: {
     layout: 'fullscreen',
   },
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = () => <Header />;
+const Template: ComponentStory<typeof Header> = () => <HeaderComponent />;
 
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    rest.get('http://localhost:6006/product/category-list', (req, res, ctx) => {
+      return res(ctx.json(categorylist));
+    }),
+  ],
+};

8b3d46049debea73249e5878ad314230eb44df18
김한철 <05.hanch.it@gmail.com>
2021-08-23 18:51:01 +0900

    [#168] :zap: feat: 스토리북 카테고리 페이지 mock api 연결

---
+++ client/src/dummies/mockCategoryData.ts
@@ -0,0 +1,267 @@
+export default {
+  message: null,
+  result: [
+    {
+      createdAt: '2021-08-19T02:03:40.394Z',
+      id: 72273,
+      title: '쁘띠팬시 일반 인덱스 라벨 견출지 20-220R',
+      price: '6190',
+      stock: 698,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/04/29/11/5/7f40f26f-c74c-4ae3-b82c-9e5bdf830a2f.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.394Z',
+      id: 72272,
+      title: '디자인아지트 방수네임스티커 혼합형22종류 모두3+3 네임스티커',
+      price: '2500',
+      stock: 617,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/30a9/22585817e243ebb69e39cb3e61bbcb4608910463be21769b385972f0b266.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.392Z',
+      id: 72271,
+      title:
+        '린블리샵 방수포토네임스티커 포토네임스티커 네임스티커 포토스티커 이름스티커',
+      price: '4500',
+      stock: 579,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/c44f/b065c5e2c4faf7298453a594410cf4914cc7aad04ea018f08a165a91465e.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.386Z',
+      id: 72278,
+      title: '[디자인맑음] 방수네임스티커 3+3 중형',
+      price: '1900',
+      stock: 434,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/fe78/04eea20ba28fb7577ba49e115c79a2ff40277d7d6f2b2083e3016f44f312.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.386Z',
+      id: 72270,
+      title: '릴홀더목걸이(고급형)+사원증케이스(001) (국내생산)특가판매',
+      price: '1800',
+      stock: 184,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/b20e/ee5af70eae0a3082ffd1b6ee5b0f28f7d7f987b21a3731fc8d4c3b86e1d4.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.377Z',
+      id: 72269,
+      title: '디자인아지트 왕의귀환 3장+3장 방수네임스티커 타원형',
+      price: '1800',
+      stock: 364,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2017/06/29/19/5/9e2b0e35-f36a-4a3d-9dd5-797380b97727.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.321Z',
+      id: 72268,
+      title: 'THE100퍼센트 메탈릭 사원증 목걸이 줄 케이스 세로형',
+      price: '6400',
+      stock: 363,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2021/01/27/14/1/52121976-8e6f-4d7f-9b59-b75ba242ece5.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.319Z',
+      id: 72267,
+      title: '미르네임 1+1 방수 네임스티커 사각(가로형) 대형',
+      price: '2100',
+      stock: 915,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2017/03/13/15/8/63af0c49-2a25-473d-9791-7ce39a7bcd34.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.319Z',
+      id: 72266,
+      title:
+        '더굿데이 원형스티커 리뷰 배달앱 배달 배민스티커 서비스 배송 포장스티커 24가지 50장100장',
+      price: '4500',
+      stock: 584,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/0945/b5b1ad9d038d781de0715bb3aa93b225813374cb494d12e01f5da8e20b4a.png',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.306Z',
+      id: 72264,
+      title: '제이명찰 아크릴집게 명찰 옷핀집게형 70 x 28 mm',
+      price: '18750',
+      stock: 828,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/product/image/vendoritem/2019/04/23/3261581353/8139cb4a-3347-44ff-843b-5b1c32b215d3.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.306Z',
+      id: 72265,
+      title: '네임코코 주문제작 혼합형 네임스티커',
+      price: '1690',
+      stock: 993,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/ba64/ddb56738eed012b533b7dcd6232e1f309120c6928cb496ff741ebf989d27.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.304Z',
+      id: 72263,
+      title: '네임코코 의류용 직사각 네임스탬프 S11',
+      price: '6900',
+      stock: 841,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/4630/916b867d0add6f55b48154484ed3c3fe249e6a976c35a4e6d843c9c6a7eb.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.301Z',
+      id: 72262,
+      title: '네임코코 소형 방수 네임스티커',
+      price: '950',
+      stock: 120,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2018/12/05/14/6/7b3775a1-021d-43e2-8a1c-59d595d6a181.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.300Z',
+      id: 72261,
+      title: '구슬 규방 볼체인 군번줄',
+      price: '1500',
+      stock: 131,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2018/07/27/10/5/ec2f802b-0d1a-4eab-b346-10d1a0124e1f.png',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.294Z',
+      id: 72260,
+      title: 'TYPE 06 원형 손글씨 스티커 1000매',
+      price: '19000',
+      stock: 229,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/f0bc/7e7307240380fbfaf8b94966b653a89915b08eabf6deb98fa5e1994c107e.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.290Z',
+      id: 72259,
+      title: '제이명찰 아크릴 명찰 옷핀집게형 중 90 x 61 mm',
+      price: '9330',
+      stock: 679,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/80534356142621-00db86da-760c-41df-9d91-ae6b406f34d5.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.288Z',
+      id: 72258,
+      title: '스마일 릴홀더 명찰고리 5세트 사원증목걸이 줄목걸이',
+      price: '5120',
+      stock: 653,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2018/10/26/17/1/40f0e138-15f4-4033-a741-4dc6495f25e6.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.288Z',
+      id: 72257,
+      title: '데이플레인 방수 네임스티커 투명 흰색',
+      price: '7900',
+      stock: 837,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/aadc/d046ef4071582208405fcb5f4c59c7c81161f4c1b22bc73408c9528aa0e1.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.287Z',
+      id: 72256,
+      title: '네임코코 원형 방수 네임스티커',
+      price: '880',
+      stock: 166,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2018/12/12/14/4/80453acd-52d8-4161-b294-dc3149af24f2.jpg',
+        },
+      ],
+    },
+    {
+      createdAt: '2021-08-19T02:03:40.286Z',
+      id: 72255,
+      title: '너스키니 간호사테이프걸이 릴홀더 [베이직-파츠X]',
+      price: '2700',
+      stock: 334,
+      discount: 0,
+      productImage: [
+        {
+          url: '//thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/1000/73fd0c39866e7c5fd92f9593446b262db2fe5875e456cf700b524ee1bb29.jpg',
+        },
+      ],
+    },
+  ],
+};
--- client/src/pages/Category/Category.tsx
+++ client/src/pages/Category/Category.tsx
@@ -7,13 +7,14 @@ import { useRecoilValue } from 'recoil';
 import { ISubCategory } from '@/types';
 
 const Category = () => {
-  const { categoryId } = useParams().params;
+  const { categoryId = '40' } = useParams().params;
   const categories = useRecoilValue(CategoryList);
 
   const sub: ISubCategory = {
     id: 0,
     title: '',
   };
+
   const main = categories.find((_main) => {
     return _main.subCategories.find((_sub) => {
       if (_sub.id == +categoryId) {

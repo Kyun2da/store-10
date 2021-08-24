8ef92c9d3757fb7f921c96a26bd8462ce0d5e50b
김한철 <05.hanch.it@gmail.com>
2021-08-25 07:53:09 +0900

    [#162] :zap: feat: 메인페이지 배너 추가, 카드 wrapper 아래 패딩 추가

--- client/src/components/CardWrapper/styles.ts
+++ client/src/components/CardWrapper/styles.ts
@@ -7,5 +7,6 @@ interface CardWrapperProps {
 export const CardWrapper = styled.ul<CardWrapperProps>`
   display: grid;
   gap: 2rem;
+  padding-bottom: 2rem;
   grid-template-columns: repeat(${({ col }) => col}, 1fr);
 `;

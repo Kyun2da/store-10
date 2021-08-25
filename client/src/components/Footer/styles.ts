87451dc03313930a39c85365559269327873e2ee
KG <acrkdduf@naver.com>
2021-08-25 21:31:42 +0900

    [#235] :sparkles: refactor: Footer 기존 4단 구조에서 3단구조로 변경 및 일부 세부 항목 제거

--- client/src/components/Footer/styles.ts
+++ client/src/components/Footer/styles.ts
@@ -29,7 +29,7 @@ export const Footer = styled.div`
 
 export const FooterTabs = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr 1fr 1fr;
+  grid-template-columns: 2fr 1fr 1fr;
   gap: 2rem;
 `;
 

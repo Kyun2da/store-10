91d813653be5dd010296ed72720a6a7a84cb01ca
dongqui <dongqui9@gmail.com>
2021-08-27 17:52:08 +0900

    [#284] :zap: feat: 배송조회 모달 추가

---
+++ client/src/components/Shared/Modal/DeliveryModal/DeliveryModal.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import * as S from './styles';
+import ModalLayout from '../ModalLayout';
+import { useUpdateOrder } from '@/hooks/queries/order';
+import Spinner from '@/components/Shared/Spinner';
+import { SelectedSVG } from '@/assets/svgs';
+import { useQueryClient } from 'react-query';
+
+interface IProps {
+  toggleModal: () => void;
+  id: number;
+}
+const DeliveryModal = ({ toggleModal, id }: IProps) => {
+  const [imgeLoaded, setImgeLoaded] = useState(false);
+  const [delivered, setDelivered] = useState(false);
+  const { mutate, isLoading } = useUpdateOrder();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (imgeLoaded) {
+      setTimeout(() => {
+        mutate(
+          {
+            order: {
+              id,
+              status: 'delivered',
+              deliveredAt: new Date(),
+            },
+          },
+          {
+            onSuccess() {
+              setDelivered(true);
+              queryClient.invalidateQueries('orders');
+            },
+          }
+        );
+      }, 3000);
+    }
+  }, [imgeLoaded, mutate, id]);
+
+  return (
+    <ModalLayout width="32rem" height="30rem" toggleModal={toggleModal}>
+      {isLoading ? (
+        <S.Wrapper>
+          <Spinner />
+        </S.Wrapper>
+      ) : delivered ? (
+        <S.Wrapper>
+          <SelectedSVG width={80} height={80} />
+          <span>때마침, 배달이 완료되었습니다.</span>
+        </S.Wrapper>
+      ) : (
+        <S.Wrapper>
+          <img
+            width={200}
+            height={200}
+            onLoad={() => {
+              setImgeLoaded(true);
+            }}
+            src="https://store-10.s3.ap-northeast-2.amazonaws.com/test/deliver.gif"
+          />
+        </S.Wrapper>
+      )}
+    </ModalLayout>
+  );
+};
+
+export default DeliveryModal;

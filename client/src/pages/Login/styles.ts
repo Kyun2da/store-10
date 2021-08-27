240d5fab9d765a77768a3cc91e904c0e8510bcc7
허균 <kyun2dot@gmail.com>
2021-08-27 11:46:02 +0900

    [#276] :nail_care style: 로그인 페이지 반응형 추가

--- client/src/pages/Login/styles.ts
+++ client/src/pages/Login/styles.ts
@@ -4,12 +4,17 @@ import { GitHubSVG } from '@/assets/svgs';
 
 export const LoginTitle = styled(Title)`
   margin-bottom: 4rem;
+
+  @media (max-width: ${({ theme }) => theme.media.phone - 1}px) {
+    ${({ theme }) => theme.fontSize.l}
+  }
 `;
 
 export const LoginForm = styled.form`
   max-width: ${({ theme }) => theme.media.pc}px;
   display: flex;
   flex-direction: column;
+  padding-top: 5rem;
   width: 420px;
   text-align: center;
   margin: auto;
@@ -22,10 +27,28 @@ export const LoginForm = styled.form`
   button {
     height: 5rem;
   }
+
+  @media (max-width: ${({ theme }) => theme.media.phone - 1}px) {
+    width: 260px;
+    button {
+      height: 3rem;
+      ${({ theme }) => theme.fontSize.s}
+      margin-bottom: 1rem;
+    }
+
+    input {
+      ${({ theme }) => theme.fontSize.s}
+      height: 3rem;
+    }
+  }
 `;
 
 export const GithubIcon = styled(GitHubSVG)`
   margin-right: 1rem;
+
+  @media (max-width: ${({ theme }) => theme.media.phone - 1}px) {
+    width: 2rem;
+  }
 `;
 
 export const LinkContainer = styled.div`
@@ -40,9 +63,17 @@ export const LinkContainer = styled.div`
     border-left: 1px solid gray;
     color: ${({ theme }) => theme.color.primary};
   }
+
+  @media (max-width: ${({ theme }) => theme.media.phone - 1}px) {
+    ${({ theme }) => theme.fontSize.xs}
+  }
 `;
 
 export const LogoImg = styled.img`
   height: 3rem;
   margin-right: 1rem;
+
+  @media (max-width: ${({ theme }) => theme.media.phone - 1}px) {
+    height: 2rem;
+  }
 `;

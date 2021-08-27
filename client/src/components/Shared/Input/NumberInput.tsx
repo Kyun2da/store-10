8bcb848982f14ceec6e373214cd4869dec21d0da
KG <acrkdduf@naver.com>
2021-08-27 22:27:26 +0900

    [#301] :sparkles: refactor: 불필요한 주석 제거

--- client/src/components/Shared/Input/NumberInput.tsx
+++ client/src/components/Shared/Input/NumberInput.tsx
@@ -29,8 +29,6 @@ const NumberInput = ({
     <S.NumberInputArea>
       <S.AdjustButton onClick={handleClickOnMinus}>-</S.AdjustButton>
       <S.NumberInput
-        // TODO: 지금은 콘솔에러 땜에 readOnly 먹여놨는데 이러면 타이핑으로 직접 수량 입력이 불가합니다
-        // 키보드 입력으로도 수량 입력을 가능하게 해줄지 (대량구매 고려 등..) 논의가 필요할듯??
         onChange={(e) => handleOnChnage(e)}
         value={value}
         type={type}

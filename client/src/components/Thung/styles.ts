69dce24d7427b6cb38c159010bbcd8471142816a
dongqui <dongqui9@gmail.com>
2021-08-28 20:17:56 +0900

    [#323] :nail_care: style: 텅 이미지 크기 동적으로

--- client/src/components/Thung/styles.ts
+++ client/src/components/Thung/styles.ts
@@ -4,6 +4,9 @@ export const ThungContainer = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
+  img {
+    width: 100%;
+  }
 `;
 
 export const Description = styled.p`
